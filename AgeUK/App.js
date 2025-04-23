113c484e1cf7b092eb6c1ae38a520e0b551e8dcd
Kiran110405 <147168552+Kiran110405@users.noreply.github.com>
2025-04-23 14:19:48 +0100

    #

--- AgeUK/App.js
+++ AgeUK/App.js
@@ -66,7 +66,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     borderWidth: 20,
-    borderTopWidth: 150,
     borderColor: "blue",
   },
   buttonOne: {
